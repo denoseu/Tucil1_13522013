15afbc3f23f171f6391995bb79a9f749ef46612d
denoseu <denisefeliciatiowanni@gmail.com>
2024-02-08 00:16:37 +0700

    Update IO.py

--- src/IO.py
+++ src/IO.py
@@ -1,3 +1,5 @@
+import random
+
 #*** read file txt ***#
 def read_file(filename):
     with open(filename, 'r') as file:
@@ -13,6 +15,42 @@ def read_file(filename):
             sequence_rewards.append(int(file.readline().strip()))
 
     return buffer_size, matrix_width, matrix_height, matrix, number_of_sequences, sequences, sequence_rewards
+
+#*** read from keyboard ***#
+def keyboard_input():
+    jumlah_token_unik = input("Jumlah token unik: ")
+    token = []
+    for i in range(int(jumlah_token_unik)):
+        token.append(input())
+
+    ukuran_buffer = input("Ukuran buffer: ")
+
+    ukuran_matriks = input("Ukuran matriks: ")
+    row_mtx, col_mtx = ukuran_matriks.split()
+    row = int(row_mtx)
+    col = int(col_mtx)
+
+    matriks = [['' for i in range(col)] for j in range(row)]
+    for j in range (row):
+        for k in range (col):
+            matriks[j][k] = random.choice(token)
+
+    jumlah_sekuens = input("Jumlah sekuens: ")
+    ukuran_maksimal_sekuens = input("Ukuran maksimal sekuens: ")
+
+    sequences = []
+    for a in range(int(jumlah_sekuens)):
+        ukuran = random.randint(2, int(ukuran_maksimal_sekuens))
+        sequence = []
+        for b in range(ukuran):
+            sequence.append(random.choice(token))
+        sequences.append(sequence)
+
+    hadiah = []
+    for c in range(int(jumlah_sekuens)):
+        hadiah.append(random.randint(1, 50))
+
+    return jumlah_token_unik, token, ukuran_buffer, col, row, matriks, jumlah_sekuens, ukuran_maksimal_sekuens, sequences, hadiah
 
 #*** write array of array ke file txt ***#
 def write_to_file(sequences, filename):
